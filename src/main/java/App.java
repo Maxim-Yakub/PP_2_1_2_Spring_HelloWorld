7a092ed0d37790befa2e22fa702541fa9aa35fcb
Work <79774488441@ya.ru>
2022-07-06 15:02:00 +0300

    test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld newHWBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean == newHWBean);
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat anotherCatBean =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(catBean == anotherCatBean);
+
     }
 }
